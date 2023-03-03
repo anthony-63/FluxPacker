038c2cc238c7f6e8f7565f2a746c771a96868ac3
anthony-63 <back.up.email.158@gmail.com>
2023-03-03 02:15:55 -0500

    Add artificial sleep suggested by lemons#8928

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#ifdef linux
+#include <unistd.h>
+#endif
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #include "mappacker.h"
 
 #define FLUX_PACKER_ART \
@@ -30,6 +38,7 @@ int main() {
     printf("Enter the map data.txt file name: ");
     scanf("%[^\n]%*c", map_data_file_name);
     printf("Now that your done with that longgg process. Let me generate the packed map file for you.\nPacking...\n");
+    sleep(1);
     flux_packer* packer = flux_packer_init(artist_name, song_name, mapper_name, map_data_file_name, audio_file_name);
     flux_packer_pack(packer);
     flux_packer_cleanup(packer);
